7f71054bca91d2708533089973bd84b7050f396d
swarui <stevewaruim@gmail.com>
2023-10-26 15:28:25 +0300

    Completed hooks event handling

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+    const handleChange = (event) => {
+      console.log("Entering password...");
+    };
     return (
-        <div></div>
-    )
-}
-
-export default Keypad;
+      <div>
+        <input type="password" onChange={handleChange} />
+      </div>
+    );
+  }
+  
+  export default Keypad;
